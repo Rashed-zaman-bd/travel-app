7fe8304c64e7cc0e501fee73beaf247a8d6d1c95
Rashed-zaman-bd <index.rashed@gmail.com>
2026-09-03 21:58:08 +0600

    New commit

---
+++ travel-front/src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+
+
+</script>
+
+<template>
+   <h1 class="text-3xl font-bold underline">
+    Hello world!  
+    
+    <i class="bi bi-airplane-fill"></i>
+      <!-- Solid icons (fas prefix) -->
+    <font-awesome-icon :icon="['fas', 'user-secret']" />
+    <font-awesome-icon :icon="['fas', 'thumbs-up']" />
+    <font-awesome-icon :icon="['fas', 'house']" />
+    <font-awesome-icon :icon="['fas', 'phone']" />
+
+    <!-- Regular icon (far prefix) -->
+    <font-awesome-icon :icon="['far', 'circle-user']" />
+
+    <!-- Brand icon (fab prefix) -->
+    <font-awesome-icon :icon="['fab', 'facebook']" />
+  </h1>
+</template>
+
+<style scoped></style>
---
+++ travel-front/src/main.ts
@@ -0,0 +1,38 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import './assets/css/main.css'
+
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+/* Import Font Awesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* Import Font Awesome component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* Import Solid Icons */
+import { faUserSecret, faThumbsUp, faHouse, faPhone } from '@fortawesome/free-solid-svg-icons'
+
+/* Import Regular Icons */
+import { faCircleUser } from '@fortawesome/free-regular-svg-icons'
+
+/* Import Brands Icons */
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+
+/* Add all imported icons to the library */
+library.add(
+  faUserSecret,
+  faThumbsUp,
+  faHouse,
+  faPhone,
+  faCircleUser,
+  faFacebook
+)
+
+const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.use(router)
+
+app.mount('#app')
